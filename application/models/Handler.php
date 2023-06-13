9d0b7df91b5e324fcbde84f0ed8188aab1f2f748
uxamatahir8 <91980259+uxamatahir8@users.noreply.github.com>
2023-06-13 21:38:36 +0500

    home controller

---
+++ application/models/Handler.php
@@ -0,0 +1,64 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class Handler extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function select($table, $column = 'id', $order_by = 'DESC', $count = FALSE)
+    {
+        $query = $this
+            ->db
+            ->order_by($column, $order_by)
+            ->get($table);
+
+        if (!$count) {
+            return $query->result_array();
+        } else {
+            return $query->num_rows();
+        }
+    }
+    public function select_where($table, $column, $condition, $count = FALSE, $single = TRUE)
+    {
+        $query = $this
+            ->db
+            ->where($column, $condition)
+            ->get($table);
+
+        if (!$count && $single) {
+            return $query->row();
+        } else if (!$single && !$count) {
+            return $query->result_array();
+        } else {
+            return $query->num_rows();
+        }
+    }
+    public function delete($table, $column = FALSE, $condition = FALSE)
+    {
+        if (!$column) {
+            $query = $this->db->delete($table);
+        } else {
+            $query = $this
+                ->db
+                ->where($column, $condition)
+                ->delete($table);
+        }
+
+        return $query;
+    }
+    public function truncate($table)
+    {
+        $this->db->truncate($table);
+    }
+
+    public function getCell($table, $column, $condition, $cell_name)
+    {
+        $query = $this
+            ->db
+            ->where($column, $condition)
+            ->get($table);
+
+        return $query->row()->$cell_name;
+    }
+}
